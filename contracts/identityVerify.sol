26899bc9a6998267473c438b052d0aeb761383d3
Baiqing Lyu <baiqinglyu@gmail.com>
2020-03-07 14:32:53 -0500

    Formatting

--- contracts/identityVerify.sol
+++ contracts/identityVerify.sol
@@ -74,7 +74,7 @@ contract identityVerification{
     {
         return patients[target].message;
     }
-    
+
     function getUserConsentExpired(
         address target
     ) 
@@ -104,7 +104,6 @@ contract identityVerification{
         }
     }
     
-    
     function ecrecovery(
         bytes32 hash,
         bytes memory sig
@@ -116,33 +115,27 @@ contract identityVerification{
         bytes32 r;
         bytes32 s;
         uint8 v;
-
         if (sig.length != 65) {
         return address(0);
-    }
-
-    assembly {
-        r := mload(add(sig, 32))
-        s := mload(add(sig, 64))
-        v := and(mload(add(sig, 65)), 255)
-    }
-
-    if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
-        return address(0);
-    }
-
-    if (v < 27) {
-        v += 27;
-    }
-
-    if (v != 27 && v != 28) {
-        return address(0);
-    }
-    
+        }
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := and(mload(add(sig, 65)), 255)
+        }
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            return address(0);
+        }
+        if (v < 27) {
+            v += 27;
+        }
+        if (v != 27 && v != 28) {
+            return address(0);
+        }
         return ecrecover(hash, v, r, s);
     }
     
-        function hash_msg(
+    function hash_msg(
         string memory _msg
     ) 
         internal
