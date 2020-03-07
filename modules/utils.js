c7bd5dc6c089343cb0108ef8ead0591f8f10e8f6
Joe Howarth <josephehowarth@gmail.com>
2020-03-07 18:56:40 -0500

    using template now...

--- modules/utils.js
+++
@@ -1,6 +0,0 @@
-/* Immuto Web App Template | (c) Immuto, Inc. and other contributors */
-
-exports.is_empty = (obj) => {
-   for (var x in obj) { return false; }
-      return true;
-}
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Dashboard from './admin-dashboard'
-
-const App = () => {
-    return <Dashboard/>
-}
-
-ReactDOM.render(<App />, document.getElementById("app"))
