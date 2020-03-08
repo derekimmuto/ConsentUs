c7dad1cdb6b966c1568fc126787410f7ef6d1a40
Joe Howarth <josephehowarth@gmail.com>
2020-03-07 20:07:13 -0500

    not sure

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,58 @@
 import React from "react"
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
-import SideBar from './sidebar'
+import TopBar from "./top-bar"
+import "bootstrap/dist/css/bootstrap.min.css"
+import SideNav, {
+    Toggle,
+    Nav,
+    NavItem,
+    NavIcon,
+    NavText
+} from "@trendmicro/react-sidenav"
+
+// Be sure to include styles at some point, probably during your bootstraping
+import "@trendmicro/react-sidenav/dist/react-sidenav.css"
 
 const App = () => {
     return (
-        // <p>hi</p>
-        <SideBar></SideBar>
+        <Router>
+            <SideNav
+                onSelect={selected => {
+                    // Add your code here
+                }}
+            >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                    <NavItem eventKey="home">
+                        <NavIcon>
+                            <i
+                                className="fa fa-fw fa-home"
+                                style={{ fontSize: "1.75em" }}
+                            />
+                        </NavIcon>
+                        <NavText>Home</NavText>
+                    </NavItem>
+                    <NavItem eventKey="charts">
+                        <NavIcon>
+                            <i
+                                className="fa fa-fw fa-line-chart"
+                                style={{ fontSize: "1.75em" }}
+                            />
+                        </NavIcon>
+                        <NavText>Charts</NavText>
+                        <NavItem eventKey="charts/linechart">
+                            <NavText>Line Chart</NavText>
+                        </NavItem>
+                        <NavItem eventKey="charts/barchart">
+                            <NavText>Bar Chart</NavText>
+                        </NavItem>
+                    </NavItem>
+                </SideNav.Nav>
+            </SideNav>
+            <TopBar/>
+        </Router>
     )
 }
 
-    
 ReactDOM.render(<App></App>, document.getElementById("app"))
